b26a19792fe0c443658727c1c007b88b1face835
Justin Riddiough <jriddiough@gmail.com>
2022-12-07 05:56:26 -0500

    image gen

--- custom-aimodels/img2img-upscale.py
+++ custom-aimodels/img2img-upscale.py
@@ -8,14 +8,14 @@ assert all(c.isalnum() or c in ['-', '_', '.'] for c in model_name)
 
 # put each filename from /tmp/outputs/samples_resized/ into array
 filenames = subprocess.check_output('ls -1 /tmp/samples_resized/samples/', shell=True).decode('utf-8').splitlines()
-
+print(filenames)
 # iterate through filenames with counter
 for i, filename in enumerate(filenames):
   # get the line from prompts.txt that corresponds to the current filename
   prompt = subprocess.check_output(f'head -n {i+1} /var/meadowrun/machine_cache/prompts.txt | tail -n 1', shell=True).decode('utf-8')
   script_args = ' --skip_grid ' \
     ' --ddim_steps 90 '\
-   f' --init-img /tmp/outputs/samples_resized/{filename} '\
+   f' --init-img /tmp/samples_resized/samples/{filename} '\
     ' --n_samples 1 '\
     ' --n_iter 1 '\
     ' --scale 7.5 '\
