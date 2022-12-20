857906c910939f14e31046793cfeff2b8882a114
Justin Riddiough <jriddiough@gmail.com>
2022-12-20 13:47:51 -0500

    refactoring paths

---
+++ visioninit/createimage.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+
+model_name = sys.argv[1]
+
+script_args = ' --skip_grid ' \
+  ' --ddim_steps 30 '\
+  ' --H 320 '\
+  ' --W 576 '\
+  ' --n_samples 1 '\
+  ' --n_iter 1 '\
+  ' --scale 8 '\
+  ' --from-file custom-aimodels/prompts.txt '\
+ f' --ckpt /var/meadowrun/machine_cache/{model_name} '\
+  ' --seed 1330 '\
+  ' --precision autocast '\
+  ' --outdir /tmp/outputs '
+
+def main(model_name):
+  subprocess.check_output('python scripts/txt2img.py ' + script_args, shell=True).decode('utf-8')
+
---
+++ visioninit/pipeline.py
@@ -0,0 +1,8 @@
+import createimage
+
+def main(model_name, bucket_name, output_path):
+    # create image
+    createimage.main(model_name, bucket_name, output_path)
+    # resize image
+    # upscale image
+    # upload image to s3
